851fa7f421da0834c7adf6cb6fd2264a426d1d20
Klayver Ximenes <klayverx@gmail.com>
2024-12-25 19:00:07 -0300

    fix: remove react-scroll-parallax

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,25 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import { Providers } from "./providers";
 
 import "./globals.css";
 
 const geistSans = Geist({
 	variable: "--font-geist-sans",
-	subsets: ["latin"]
+	subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
 	variable: "--font-geist-mono",
-	subsets: ["latin"]
+	subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
 	title: "Klayver Ximenes",
-	description: "Portfólio Klayver Ximenes"
+	description: "Portfólio Klayver Ximenes",
 };
 
 export default function RootLayout({
-	children
+	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
@@ -29,7 +28,7 @@ export default function RootLayout({
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
-				<Providers>{children}</Providers>
+				{children}
 			</body>
 		</html>
 	);
