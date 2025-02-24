77e9f6ed322fb253e14439a611b99bb6c22393c4
Klayver Ximenes <klayverx@gmail.com>
2025-02-24 16:48:37 -0300

    refactor: remove smooth scroll behavior from global styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en" className="scroll-smooth">
+		<html lang="en">
 			<body
 				className={`${koulenRegular.variable} antialiased`}
 			>
