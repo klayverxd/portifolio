e47c9a6e38a98792c65d65acfb5dcf1bfc2a7cc7
Klayver Ximenes <klayverx@gmail.com>
2025-02-26 16:24:06 -0300

    feat: configure remote image patterns for Next.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 const nextConfig = {
-  /* config options here */
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "topic-coreau.vercel.app",
+			},
+		],
+	},
 };
 
 export default nextConfig;
