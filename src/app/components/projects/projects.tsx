e489203f5c2eaa0d9d66b818895ff0d96a6168ab
Klayver Ximenes <klayverx@gmail.com>
2025-02-26 22:37:12 -0300

    feat: move project data to constants for better organization

--- src/app/components/projects/projects.tsx
+++ src/app/components/projects/projects.tsx
@@ -1,33 +1,7 @@
 import React, { useRef } from "react";
 import Card from "./card";
 import { useScroll } from "motion/react";
-
-const projects = [
-	{
-		title: "Project 1",
-		description:
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam metus libero, ullamcorper ac ultricies quis, feugiat vel turpis. Proin aliquet felis ac est luctus accumsan. Maecenas lorem dolor, dapibus et nunc vitae, mattis maximus mauris. Nam neque quam, accumsan vel ex in, aliquam ornare risus.",
-		src: "itatiaia.png",
-		url: "https://www.klayver.dev",
-		color: "#BBACAF",
-	},
-	{
-		title: "Project 2",
-		description:
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam metus libero, ullamcorper ac ultricies quis, feugiat vel turpis. Proin aliquet felis ac est luctus accumsan. Maecenas lorem dolor, dapibus et nunc vitae, mattis maximus mauris. Nam neque quam, accumsan vel ex in, aliquam ornare risus.",
-		src: "itatiaia.png",
-		url: "https://www.klayver.dev",
-		color: "#977F6D",
-	},
-	{
-		title: "Project 3",
-		description:
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam metus libero, ullamcorper ac ultricies quis, feugiat vel turpis. Proin aliquet felis ac est luctus accumsan. Maecenas lorem dolor, dapibus et nunc vitae, mattis maximus mauris. Nam neque quam, accumsan vel ex in, aliquam ornare risus.",
-		src: "itatiaia.png",
-		url: "https://www.klayver.dev",
-		color: "#C2491D",
-	},
-];
+import { projects } from "@/app/constants/projects";
 
 export default function Projects() {
 	const container = useRef(null);
@@ -45,7 +19,7 @@ export default function Projects() {
 
 				return (
 					<Card
-						key={`p_${i}`}
+						key={`p_${project.title}`}
 						i={i}
 						{...project}
 						progress={scrollYProgress}
---
+++ src/app/constants/projects.ts
@@ -0,0 +1,58 @@
+export const projects = [
+	{
+		title: "Itatiaia",
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam metus libero, ullamcorper ac ultricies quis, feugiat vel turpis. Proin aliquet felis ac est luctus accumsan. Maecenas lorem dolor, dapibus et nunc vitae, mattis maximus mauris. Nam neque quam, accumsan vel ex in, aliquam ornare risus.",
+		src: "itatiaia.png",
+		url: "https://www.klayver.dev",
+		color: "#FF153E",
+	},
+	{
+		title: "Loovi",
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam metus libero, ullamcorper ac ultricies quis, feugiat vel turpis. Proin aliquet felis ac est luctus accumsan. Maecenas lorem dolor, dapibus et nunc vitae, mattis maximus mauris. Nam neque quam, accumsan vel ex in, aliquam ornare risus.",
+		src: "loovi.png",
+		url: "https://www.klayver.dev",
+		color: "#5A78FF",
+	},
+	{
+		title: "Needyu",
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam metus libero, ullamcorper ac ultricies quis, feugiat vel turpis. Proin aliquet felis ac est luctus accumsan. Maecenas lorem dolor, dapibus et nunc vitae, mattis maximus mauris. Nam neque quam, accumsan vel ex in, aliquam ornare risus.",
+		src: "needyu.png",
+		url: "https://www.klayver.dev",
+		color: "#1D3049",
+	},
+	{
+		title: "Embrapa",
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam metus libero, ullamcorper ac ultricies quis, feugiat vel turpis. Proin aliquet felis ac est luctus accumsan. Maecenas lorem dolor, dapibus et nunc vitae, mattis maximus mauris. Nam neque quam, accumsan vel ex in, aliquam ornare risus.",
+		src: "embrapa.png",
+		url: "https://www.klayver.dev",
+		color: "#378254",
+	},
+	{
+		title: "Brasil 247",
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam metus libero, ullamcorper ac ultricies quis, feugiat vel turpis. Proin aliquet felis ac est luctus accumsan. Maecenas lorem dolor, dapibus et nunc vitae, mattis maximus mauris. Nam neque quam, accumsan vel ex in, aliquam ornare risus.",
+		src: "brasil247.png",
+		url: "https://www.klayver.dev",
+		color: "#215181",
+	},
+	{
+		title: "Dell Lead",
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam metus libero, ullamcorper ac ultricies quis, feugiat vel turpis. Proin aliquet felis ac est luctus accumsan. Maecenas lorem dolor, dapibus et nunc vitae, mattis maximus mauris. Nam neque quam, accumsan vel ex in, aliquam ornare risus.",
+		src: "lead.png",
+		url: "https://www.klayver.dev",
+		color: "#0076CE",
+	},
+	{
+		title: "Med Aula",
+		description:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam metus libero, ullamcorper ac ultricies quis, feugiat vel turpis. Proin aliquet felis ac est luctus accumsan. Maecenas lorem dolor, dapibus et nunc vitae, mattis maximus mauris. Nam neque quam, accumsan vel ex in, aliquam ornare risus.",
+		src: "medaula.png",
+		url: "https://www.klayver.dev",
+		color: "#FF0100",
+	},
+];
